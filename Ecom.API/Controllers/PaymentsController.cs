5785f0a4a84fd19b2566079b7c97a0634796d1c1
mostafa <mostafaelanany45@gmail.com>
2026-02-03 04:02:34 +0200

    Initial clean commit (no secrets)

---
+++ Ecom.API/Controllers/PaymentsController.cs
@@ -0,0 +1,72 @@
+﻿using Ecom.Core.Entities;
+using Ecom.Core.Entities.Order;
+using Ecom.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Ecom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IPaymentService paymentService;
+
+        public PaymentsController(IPaymentService paymentService)
+        {
+            this.paymentService = paymentService;
+        }
+
+
+
+        // Create payment method
+        [Authorize]
+        [HttpPost("Create")]
+        public async Task<ActionResult<CustomerBasket>> Create(string basketId, int? deliveryId)
+        {
+            return await paymentService.CreateOrUpdatePaymentAsync(basketId, deliveryId);
+        }
+
+        const string endpointSecret = "whsec_18cf00274fc91bac91ad87d48a318da79aa3ba28977519d87351a7fa0a99cf8f";
+
+        // Webhook for handling Stripe events
+        [HttpPost("webhook")]
+        public async Task<IActionResult> UpdateStatusWithStripe()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            try
+            {
+                var stripeEvent = EventUtility.ConstructEvent(json,
+                    Request.Headers["Stripe-Signature"], endpointSecret, throwOnApiVersionMismatch: false);
+                PaymentIntent intent;
+                Orders orders;
+                // Handle the event
+                if (stripeEvent.Type == "payment_intent.payment_failed")
+                {
+                    intent = stripeEvent.Data.Object as PaymentIntent;
+                    orders = await paymentService.UpdateOrderFaild(intent.Id);
+                }
+                else if (stripeEvent.Type == "payment_intent.succeeded")
+                {
+                    intent = stripeEvent.Data.Object as PaymentIntent;
+                    orders = await paymentService.UpdateOrderSuccess(intent.Id);
+                }
+                // ... handle other event types
+                else
+                {
+                    Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+                }
+
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Ecom.API/Helper/ResponseAPI.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Ecom.API.Helper
+{
+    public class ResponseAPI
+    {
+        public ResponseAPI(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetMessageFormStatusCode(StatusCode);
+        }
+        private string GetMessageFormStatusCode(int statuscode)
+        {
+            return statuscode switch
+            {
+                200 => "OK",
+                201 => "Created",
+                202 => "Accepted",
+                204 => "No Content",
+                301 => "Moved Permanently",
+                302 => "Found",
+                304 => "Not Modified",
+                307 => "Temporary Redirect",
+                308 => "Permanent Redirect",
+                400 => "Bad Request",
+                401 => "Unauthorized",
+                403 => "Forbidden",
+                404 => "Not Found",
+                405 => "Method Not Allowed",
+                408 => "Request Timeout",
+                409 => "Conflict",
+                410 => "Gone",
+                415 => "Unsupported Media Type",
+                429 => "Too Many Requests",
+                500 => "Internal Server Error",
+                501 => "Not Implemented",
+                502 => "Bad Gateway",
+                503 => "Service Unavailable",
+                504 => "Gateway Timeout",
+                _ => "Unknown Status Code"
+            };
+        }
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+    }
+
+}
